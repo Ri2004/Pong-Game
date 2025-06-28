e01905522f68cc2ef778d847182390137ea1be98
Rishabh Chatterjee <rishabhchatterjee813@gmail.com>
2025-06-28 10:53:32 +0530

    Bouncing ball when collide with top and bottom wall

--- ball.py
+++ ball.py
@@ -8,8 +8,13 @@ class Ball(Turtle):
         self.color("white")
         self.penup()
         self.shapesize(stretch_len=1, stretch_wid=1)
+        self.x_move = 10
+        self.y_move = 10
 
     def move(self):
-        new_x = self.xcor() + 10
-        new_y = self.ycor() + 10
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
         self.goto(x=new_x, y=new_y)
+
+    def bounce(self):
+        self.y_move *= -1
